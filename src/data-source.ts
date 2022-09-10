f4646edc5cea96d556553957e87c3e7253ba2cf2
VaquarShaikh <vaquarshaikh253@gmail.com>
2022-09-10 18:28:20 +0530

    made resolvers and schema in a separate file .

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,17 +1,17 @@
-import "reflect-metadata"
-import { DataSource } from "typeorm"
-import { User } from "./entity/User"
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { User } from "./entity/User";
 
 export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "test",
-    password: "test",
-    database: "test",
-    synchronize: true,
-    logging: false,
-    entities: [User],
-    migrations: [],
-    subscribers: [],
-})
+  type: "postgres",
+  host: "localhost",
+  port: 5432,
+  username: "postgres",
+  password: "@Vaquar7113",
+  database: "graphql",
+  synchronize: true,
+  logging: true,
+  entities: ["src/entity/**/*.ts"],
+  migrations: ["src/migration/**/*.ts"],
+  subscribers: ["src/subscriber/**/*.ts"],
+});
