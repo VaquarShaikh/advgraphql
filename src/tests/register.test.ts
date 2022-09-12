0116f43509f6941f63da9e55da72fb5fa163c0b5
VaquarShaikh <vaquarshaikh253@gmail.com>
2022-09-12 16:28:56 +0530

    rookie testing done xD

--- src/tests/register.test.ts
+++ src/tests/register.test.ts
@@ -3,8 +3,10 @@ import { describe, expect, test } from "@jest/globals";
 import { startServer } from "..";
 import { request } from "graphql-request";
 import { host } from "./constants";
+import { AppDataSource } from "../data-source";
+import { User } from "../entity/User";
 
-const email = "bob@bob.com";
+const email = "abc@bob.com";
 const password = "abcd1234";
 
 const mutation = `
@@ -13,8 +15,17 @@ mutation{
 }
 `;
 
-test("adds 1 + 2 to 3", async () => {
-  // expect(sum(1, 2)).toBe(3);
+test("Registration part", async () => {
   const response = await request(host, mutation);
   expect(response).toEqual({ register: true });
+  await AppDataSource.initialize();
+  const users = await User.find({ where: { email } });
+  expect(users).toHaveLength(1);
+  const user = users[0];
+  expect(user.email).toEqual(email);
+  expect(user.password).not.toEqual(password);
 });
+
+// use a test database
+// whatever you do , just fucking drop the data/
+// the server must start within the test file
